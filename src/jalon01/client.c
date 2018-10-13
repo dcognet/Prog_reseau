2cd47cfd90f1429e42673554bd296fe25fbfd0b5
dcognet <36398384+dcognet@users.noreply.github.com>
2018-10-13 12:49:53 +0200

    /msgall avec les pseudo de l'envoyer

--- src/jalon01/client.c
+++ src/jalon01/client.c
@@ -7,6 +7,10 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <time.h>
+
+#define MSG_SIZE 256
+
 
 //Fonctions---------------------------------------------------------------------
 
@@ -16,22 +20,17 @@ void error(const char *msg)
   exit(1);
 }
 
-
+//------------------------------------------------------------------------------
 
 
 int do_socket(int domain, int type, int protocol) {
   int socket1;
   int yes = 1;
 
-  //create the socket1
   socket1 = socket(domain,type,protocol);
-
-  //check for socket1 validity
   if(socket1 == -1){
     error("ERROR socket1 creation");
   }
-
-  // set socket1 option, to prevent "already in use" issue when rebooting the server right on
   if (setsockopt(socket1, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
     error("ERROR setting socket options");
   }
@@ -47,19 +46,10 @@ void get_addr_info(const char* port, struct sockaddr_in* serv_addr,const char* h
 
   int portno;
 
-  //clean the serv_add structure
   memset(serv_addr,'\0',sizeof(serv_addr));
-
-  //cast the port from a string to an int
   portno = atoi(port);
-
-  //internet family protocol
   serv_addr->sin_family = AF_INET;
-
-  //
   inet_aton(host,&(serv_addr->sin_addr));
-
-  //we bind on the tcp port specified
   serv_addr->sin_port = htons(portno);
 
 }
@@ -87,86 +77,115 @@ void handle_client_message(int socket, const void *message){
 //------------------------------------------------------------------------------
 
 void do_read(int socket, char *buffer){
-  int i = read(socket,buffer,255);
+  memset (buffer, '\0', MSG_SIZE);
+  int i = read(socket,buffer,MSG_SIZE);
   if(i == -1){
     error("ERROR read server");
   }
 }
 
 
+
 //Corps-------------------------------------------------------------------------
 
 
 int main(int argc,char** argv){
-  char buffer[256];
-
   if (argc != 3)
   {
-    fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
+    fprintf(stderr,"usage: RE216_CLIENT  port hostname\n");
     return 1;
   }
 
+  //Variables--------------------------------------------------------------
 
-    //get the socket--------------------------------------------------------------
-    printf("Etape : Création socket\n");
-    int socket = do_socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+  char buffer[MSG_SIZE];
+  char saisie[MSG_SIZE];
+  char pseudo[MSG_SIZE];
+  char copie[MSG_SIZE];
+  int socket;
+  struct sockaddr_in pointeur_serv_addr;
+  const void* msg = copie;
 
+  //get the socket--------------------------------------------------------------
+  printf("Etape : Création socket\n");
+  socket = do_socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
-    //init the serv_add structure-------------------------------------------------
-    printf("Etape : Informations serveur\n");
-    struct sockaddr_in pointeur_serv_addr;
-    get_addr_info(argv[1], &pointeur_serv_addr,argv[2]);
+  //init the serv_add structure-------------------------------------------------
+  printf("Etape : Informations serveur\n");
+  get_addr_info(argv[1], &pointeur_serv_addr,argv[2]);
 
-
-    //connect to remote socket----------------------------------------------------
-    printf("Connexion au serveur\n");
-    do_connect(socket,pointeur_serv_addr);
-    do_read(socket,buffer);
-    printf("%s\n",buffer);
-
-
-    pid_t pid=fork();
-    char pseudo[20];
-    int i=0;
-
-    while(1){
+  //connect to remote socket----------------------------------------------------
+  printf("Connexion au serveur\n");
+  do_connect(socket,pointeur_serv_addr);
 
 
-// idée : les n 1ERE case du message sont pour dire qui envoie
+  //Obligation identification
+  memset(buffer,'\0',MSG_SIZE);
+  do_read(socket,buffer);
+  printf("%s\n",buffer);
 
-//send message to the server--------------------------------------------------
+
+
+  int i=0;
+
+  while(1){
+    memset(saisie,'\0',MSG_SIZE);
+    do_read(STDIN_FILENO,saisie);
+    //strcpy(saisie,"/nick ");
+    //strcat(saisie,argv[3]);
+    if(strncmp(saisie,"/nick ",strlen("/nick "))==0){
+      memset (copie, '\0', MSG_SIZE);
+      strncpy(copie,saisie,strlen(saisie)-1);
+      handle_client_message(socket,copie);
+      printf("Identification réussie\n");
+      break;
+    }
+    else{
+      printf("Identification échec : veuillez respecter la synthaxe\n");
+    }
+  }
+
+  do_read(socket,saisie);
+  printf("%s\n",saisie);
+
+  pid_t pid=fork();
+
+  while(1){
+
 
     if(pid==0){
-            //read what the client has to say---------------------------------------------
-            memset (buffer, '\0', sizeof (buffer));
-            do_read(socket,buffer);
-            memset (pseudo, '\0', sizeof (pseudo));
-            i=0;
-            while(buffer[i]!='|'){
-              i++;
-            }
-            strncpy(pseudo,buffer,i);
-            fprintf(stdout,"[%s] %s\n",pseudo,buffer+i+1);
+      //read what the client has to say---------------------------------------------
+      memset (buffer, '\0', sizeof (buffer));
+      do_read(socket,buffer);
+      memset (pseudo, '\0', sizeof (pseudo));
+      i=0;
+      while(buffer[i]!='|'){
+        i++;
+      }
+      strncpy(pseudo,buffer,i);
+      fprintf(stdout,"dddd\n");
+
+      fprintf(stdout,"[%s] %s\n",pseudo,buffer+i+1);
     }
     else{
       //get user input--------------------------------------------------------------
-      const char saisie[256];
-      gets(saisie);
-      const void* msg = saisie;
+      do_read(STDIN_FILENO,saisie);
+      memset (copie, '\0', MSG_SIZE);
+      strncpy(copie,saisie,strlen(saisie)-1);
+      msg = copie;
+
+      //send message to the server--------------------------------------------------
       handle_client_message(socket,msg);
+
       //connexion end---------------------------------------------------------------
-      if(strcmp(msg, "/quit") == 0 ){
+      if(strncmp(msg, "/quit",strlen("/quit")-1) == 0){
         printf("Fermeture connexion client\n");
         break;
       }
 
     }
-
-
-
-
-
   }
+
 
   //close socket----------------------------------------------------------------
   printf("Fermeture socket\n");
